ca025a40c0a521fc533cc4da5d3db22b070a03b6
Rikhardur Bjarni Einarsson <rikhardur.einarsson@disguise.one>
2021-05-20 11:24:29 +0100

    Move all visibility logic to Definition component. Remove Visibility component. Add UI to bulk add outliner/viewport selection to the visibility sets in Definition component. Closes #2

--- Source/RenderStreamEditor/Public/RenderStreamCustomization.h
+++ Source/RenderStreamEditor/Public/RenderStreamCustomization.h
@@ -3,6 +3,5 @@
 #include "IDetailCustomization.h"
 #include "DetailCategoryBuilder.h"
 
-static TSharedRef<IDetailCustomization> MakeVisibilityCustomizationInstance();
 static TSharedRef<IDetailCustomization> MakeDefinitionCustomizationInstance();
 static TSharedRef<IDetailCustomization> MakeSettingsCustomizationInstance();
