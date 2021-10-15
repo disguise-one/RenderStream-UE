ec38fbf232a82bc07eb98fcc2886ddcb86faa25b
Tom Whittock <tom.whittock@disguise.one>
2021-10-15 14:43:07 +0100

    Update rs_logToD3 usage to conform to r1.28 atomic send requirement.

--- Source/RenderStream/Public/RenderStreamLogOutputDevice.h
+++ Source/RenderStream/Public/RenderStreamLogOutputDevice.h
@@ -25,10 +25,11 @@ protected:
     {
         if (RenderStreamLink::instance().isAvailable())
         {
-            RenderStreamLink::instance().rs_logToD3(TCHAR_TO_ANSI(*Category.ToString()));
-            RenderStreamLink::instance().rs_logToD3(": ");
-            RenderStreamLink::instance().rs_logToD3(TCHAR_TO_ANSI(Message));
-            RenderStreamLink::instance().rs_logToD3("\n");
+            FString s;
+            s += Category.ToString();
+            s += ": ";
+            s += Message;
+            RenderStreamLink::instance().rs_logToD3(TCHAR_TO_ANSI(*s));
         }
     }
 
