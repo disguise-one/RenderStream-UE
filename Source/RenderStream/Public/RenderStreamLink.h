a68ff8611694be47d4f58be67b671bb921cabb6a
Chris Nash <chris.nash@disguise.one>
2023-11-03 16:50:24 +0000

    Update changelog and plugin version for RS2.0-UE5.1-v1

--- Source/RenderStream/Public/RenderStreamLink.h
+++ Source/RenderStream/Public/RenderStreamLink.h
@@ -28,7 +28,7 @@ typedef uint64_t VkDeviceSize;
 typedef struct VkSemaphore_T* VkSemaphore;
 
 #define RS_PLUGIN_NAME "RenderStream-UE"
-#define RS_PLUGIN_VERSION "RS2.0-UE5.1-v0"
+#define RS_PLUGIN_VERSION "RS2.0-UE5.1-v1"
 
 class RenderStreamLink
 {
