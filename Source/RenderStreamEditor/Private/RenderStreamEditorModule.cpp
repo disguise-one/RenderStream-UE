70569b647bf39e4a4f9d2f6cad0bd6a946339e77
ivaylo.parvanov <ivaylo.parvanov@disguise.one>
2024-12-11 16:09:53 -0500

    caches: remove unused code

--- Source/RenderStreamEditor/Private/RenderStreamEditorModule.cpp
+++ Source/RenderStreamEditor/Private/RenderStreamEditorModule.cpp
@@ -502,25 +502,6 @@ bool CheckOutLevelChannelCaches(TArray<ULevel*> Levels)
     return checkoutNotCancelled && allPackagesWriteable;
 }
 
-bool CheckOutLevelChannelCaches(TArray<ULevelStreaming*> StreamingLevels)
-{
-	
-    TArray<ULevel*> Levels;
-    for (const auto& StreamingLevel : StreamingLevels)
-        if (StreamingLevel->IsLevelLoaded())
-            Levels.Add(StreamingLevel->GetLoadedLevel());
-
-    return CheckOutLevelChannelCaches(Levels);
-}
-
-bool CheckOutLevelChannelCaches(ULevel* Level)
-{
-    TArray<ULevel*> Levels;
-	Levels.Add(Level);
-
-    return CheckOutLevelChannelCaches(Levels);
-}
-
 URenderStreamChannelCacheAsset* UpdateLevelChannelCache(ULevel* Level)
 {
     URenderStreamChannelCacheAsset* Cache = GetOrCreateCache(Level);
