646addf28f07a1a1b9193ac6c45f5ed63a6da018
unknown <josh.armstrong@disguise.one>
2025-06-05 11:50:44 -0400

    Flushing command list before getting the texture parameters will prevent the 2 threads accessing the same context error

--- Source/RenderStream/Private/RenderStreamSceneSelector.cpp
+++ Source/RenderStream/Private/RenderStreamSceneSelector.cpp
@@ -687,6 +687,10 @@ void RenderStreamSceneSelector::ApplyParameters(AActor* Root, uint64_t specHash,
                     RenderStreamLink::SenderFrame data = {};
                     if (toggle == "D3D11")
                     {
+                        {
+                            SCOPED_DRAW_EVENTF(RHICmdList, MediaCapture, TEXT("RS Tex Param Flush"));
+                            RHICmdList.ImmediateFlush(EImmediateFlushType::FlushRHIThreadFlushResources);
+                        }
                         data.type = RenderStreamLink::SenderFrameType::RS_FRAMETYPE_DX11_TEXTURE;
                         data.dx11.resource = static_cast<ID3D11Resource*>(resource);
                         auto err = RenderStreamLink::instance().rs_getFrameImage2(frameData.imageId, &data);
