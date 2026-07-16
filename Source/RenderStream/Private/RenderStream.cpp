f944a69af8fbd2e9f9660684c37a18c7e2c1ddae
josha-disguise <josh.armstrong@disguise.one>
2026-07-16 14:21:05 -0400

    Adding null check for root actor pointer.

--- Source/RenderStream/Private/RenderStream.cpp
+++ Source/RenderStream/Private/RenderStream.cpp
@@ -820,7 +820,7 @@ void FRenderStreamModule::OnBeginFrame()
     // The types are just random ones I picked
     // Epic is aware of this and is planning to fix it in 5.7.1
     ADisplayClusterRootActor* const RootActor = IDisplayCluster::Get().GetGameMgr()->GetRootActor();
-    if (RootActor->GetConfigData()->GetNode(ClusterMgr->GetNodeId())->MediaSettings.MediaOutputs.Num() == 0)
+    if (RootActor && RootActor->GetConfigData()->GetNode(ClusterMgr->GetNodeId())->MediaSettings.MediaOutputs.Num() == 0)
     {
         TObjectPtr<UFileMediaOutput> MediaOutput = NewObject<UFileMediaOutput>(GetTransientPackage());
         RootActor->GetConfigData()->GetNode(ClusterMgr->GetNodeId())->MediaSettings.bEnable = true;
