92e5851de2ae35cf92577721943d797577bba9fd
josharms-disguise <152881235+josharms-disguise@users.noreply.github.com>
2026-09-18 09:36:47 -0400

    Using the backup and restore method instead to avoid and other plugin configurations

--- Source/RenderStreamEditor/Public/RenderStreamEditorModule.h
+++ Source/RenderStreamEditor/Public/RenderStreamEditorModule.h
@@ -45,6 +45,9 @@ private:
 
     FString GetSelectedOutputFolder();
 
+    FString ShippingConfigPath() const;
+    void RemoveStaleShippingConfig();
+
     TWeakObjectPtr<UWorld> GameWorld;
     bool DirtyAssetMetadata = false;
 };
